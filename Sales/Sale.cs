ad7f0628ab6390be1616e0a39e6ef5182305af3b
MateoLindarte <69722153+MateoLindarte@users.noreply.github.com>
2021-02-17 20:44:49 -0500

    main

---
+++ Sales/Sale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    class Sale
+    {
+        public string product;
+        public string seller;
+        public string comments;
+    }
+}
